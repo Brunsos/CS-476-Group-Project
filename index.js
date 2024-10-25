4c727f5df01171bf51a51de67c4b61f63e77ca68
sen <senhuang60@gmail.com>
2024-10-24 18:14:44 -0600

    change index to root

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import app from './config/app.js'; // Import the app from app.js
+import app from './backend/config/app.js'; // Import the app from app.js
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
